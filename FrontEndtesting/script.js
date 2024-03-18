61f0bfda21d52dfa5ddc9255d0e32d253a1e91af
Adrian Contreras <adrianf1995@yahoo.com>
2024-03-18 10:29:04 -0400

    update

--- FrontEndtesting/script.js
+++ FrontEndtesting/script.js
@@ -5,21 +5,21 @@ console.log (connectorSelectorBoxes);
 
 for(selectBox of connectorSelectorBoxes){
     selectBox.addEventListener("change", function(e){
-        console.log(e);
-        console.log(e.target);
-        console.log(e.target.checked);
         for(bubble of connectorBubbles){
             bubble.classList.remove("connector_bubble_active");
         };
-
         selectedBubble = document.getElementById(e.target.value);
-        console.log(selectedBubble);
-            selectedBubble.classList.add("connector_bubble_active");
+        selectedBubble.classList.add("connector_bubble_active");
     });
 };
 
 for(selectBubble of connectorBubbles){
     selectBubble.addEventListener("click", function(e){
-        console.log("selectBubble")
+        for(bubble of connectorBubbles){
+            bubble.classList.remove("connector_bubble_active");
+        };
+        selectedBox = document.querySelector(`[value=${e.target.id}]`);
+        selectedBox.checked = true;
+        e.target.classList.add("connector_bubble_active");
     });
 }
