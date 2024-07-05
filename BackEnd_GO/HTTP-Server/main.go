32dbe56e1b8de046853a2052a0068ad1ef4aecb3
Adrian Contreras <adrianf1995@yahoo.com>
2024-07-05 12:53:58 -0400

    update

--- BackEnd_GO/HTTP-Server/main.go
+++ BackEnd_GO/HTTP-Server/main.go
@@ -7,8 +7,8 @@ func main() {
 		w.Write([]byte("Hello World"))
 	})
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
+	http.HandleFunc("/editor", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./Main front end/index.html")
 	})
 	http.ListenAndServe(":8080", nil)
 }
