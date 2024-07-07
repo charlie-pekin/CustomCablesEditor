5063130457f9f19d08cd82c951dde87f15522c87
Adrian Contreras <adrianf1995@yahoo.com>
2024-07-06 22:52:51 -0400

    update

--- BackEnd_GO/HTTP-Server/main.go
+++ BackEnd_GO/HTTP-Server/main.go
@@ -7,8 +7,11 @@ func main() {
 		w.Write([]byte("Hello World"))
 	})
 
-	http.HandleFunc("/editor", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./Main front end/index.html")
-	})
+	// http.HandleFunc("/editor", func(w http.ResponseWriter, r *http.Request) {
+	// 	http.ServeFile(w, r, "./Main front end/index.html")
+	// })
+	// http.Handle("/", http.FileServer(http.Dir("./editor")))
+	http.Handle("/", http.FileServer(http.Dir("./home")))
+	http.Handle("/editor/", http.StripPrefix("/editor/", http.FileServer(http.Dir("./editor"))))
 	http.ListenAndServe(":8080", nil)
 }
---
+++ BackEnd_GO/Test-Server/test.go
@@ -0,0 +1,10 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./home")))
+
+	http.Handle("/editor/", http.StripPrefix("/editor/", http.FileServer(http.Dir("./editor"))))
+	http.ListenAndServe(":8080", nil)
+}
